528bb072c59dd133a8cc4df2e8aef4bada0f13b1
nishu kumar <kumarnishu437@gmail.com>
2023-12-06 21:51:30 +0530

    updated

--- app/login.tsx
+++ app/login.tsx
@@ -1,10 +1,9 @@
-import { View, Image } from 'react-native'
+import { View } from 'react-native'
 import React, { useContext } from 'react'
 import { Stack } from 'expo-router';
 import { UserContext } from '../contexts/UserContext';
 import LoginForm from '../components/forms/LoginForm';
 import { LoadingContext } from '../contexts/LoadingContext';
-import { AntDesign } from '@expo/vector-icons';
 
 const login = () => {
   const { user } = useContext(UserContext)
@@ -14,7 +13,6 @@ const login = () => {
   if (!loading && !user)
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <AntDesign name="stepforward" size={24} color="black" />
         <LoginForm />
       </View>
     )
