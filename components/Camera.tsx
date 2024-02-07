03296767a49922b981f51adc824d7e2f8827b103
unknown <kumarnishu437@gmail.com>
2024-02-07 10:19:08 +0530

    updated camera torch and zoom

--- components/Camera.tsx
+++ components/Camera.tsx
@@ -2,7 +2,6 @@ import { useEffect, useRef, useState } from 'react';
 import { Image, Text, TouchableOpacity, View } from 'react-native';
 import { Camera, CameraCapturedPicture, CameraType, FlashMode } from 'expo-camera';
 import { ActivityIndicator, IconButton, MD2Colors } from 'react-native-paper';
-import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 
 type Props = {
     isLoading: boolean,
@@ -12,11 +11,10 @@ type Props = {
 }
 function CameraComponent({ isLoading, handlePress, photo, setPhoto }: Props) {
     const [flashlight, setFlashLight] = useState(FlashMode.off)
-    const [zoom, setZoom] = useState(0)
+    const [zoom, setZoom] = useState(0.10 * 0)
     const [type, setType] = useState(CameraType.back);
     const [permission, requestPermission] = Camera.useCameraPermissions();
     const cameraRef = useRef<Camera | any>()
-    const pan = Gesture.Pan();
     async function onClickPicure() {
         if (!cameraRef) return;
         const camera: Camera = cameraRef.current;
@@ -26,9 +24,11 @@ function CameraComponent({ isLoading, handlePress, photo, setPhoto }: Props) {
 
     useEffect(() => {
         requestPermission()
+        if (!permission?.granted) {
+            requestPermission()
+        }
     }, [])
-
-
+    console.log(zoom)
     return (
         <>
 
@@ -68,7 +68,6 @@ function CameraComponent({ isLoading, handlePress, photo, setPhoto }: Props) {
                                 ratio='16:9'
                                 flashMode={flashlight}
                                 type={type} ref={cameraRef}>
-    
                             </Camera>
                             <View style={{ flexDirection: 'row', height: '100%', justifyContent: 'space-evenly', backgroundColor: MD2Colors.blue400 }}>
                                 <TouchableOpacity>
@@ -82,6 +81,8 @@ function CameraComponent({ isLoading, handlePress, photo, setPhoto }: Props) {
                                         }}
                                     />
                                 </TouchableOpacity>
+
+
                                 <TouchableOpacity>
                                     <IconButton
                                         disabled={isLoading}
@@ -99,6 +100,34 @@ function CameraComponent({ isLoading, handlePress, photo, setPhoto }: Props) {
                                 </TouchableOpacity>
                                 <TouchableOpacity>
                                     <IconButton
+                                        disabled={isLoading || zoom === 1}
+                                        icon={"plus-circle"}
+                                        iconColor={MD2Colors.black}
+                                        size={40}
+                                        onPress={() => {
+                                            if (zoom > 0.9)
+                                                setZoom(1)
+                                            else
+                                                setZoom(zoom + 0.1 * 1)
+                                        }}
+                                    />
+                                </TouchableOpacity>
+                                <TouchableOpacity>
+                                    <IconButton
+                                        disabled={isLoading || zoom === 0}
+                                        icon="minus-circle-outline"
+                                        iconColor={MD2Colors.black}
+                                        size={40}
+                                        onPress={() => {
+                                            if (zoom < 0.1)
+                                                setZoom(0)
+                                            else
+                                                setZoom(zoom - 0.1 * 1)
+                                        }}
+                                    />
+                                </TouchableOpacity>
+                                <TouchableOpacity>
+                                    <IconButton
                                         disabled={isLoading}
                                         icon="camera"
                                         iconColor={MD2Colors.black}
@@ -106,6 +135,7 @@ function CameraComponent({ isLoading, handlePress, photo, setPhoto }: Props) {
                                         onPress={onClickPicure}
                                     />
                                 </TouchableOpacity>
+
                             </View>
                         </View>
                     }
