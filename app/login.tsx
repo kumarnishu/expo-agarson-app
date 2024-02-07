199f72f0c3de16362909c7cd39f21c13d5918c98
unknown <kumarnishu437@gmail.com>
2024-02-07 10:52:15 +0530

    updated login form

--- app/login.tsx
+++ app/login.tsx
@@ -1,7 +1,5 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../contexts/UserContext';
-import { Formik } from 'formik'
-import * as yup from "yup";
 import { Login } from '../services/UserServices';
 import { Image, ScrollView, View } from 'react-native';
 import { Button, MD2Colors, Snackbar, Text, TextInput } from 'react-native-paper';
@@ -10,29 +8,48 @@ import { AxiosResponse } from 'axios';
 import { IUser } from '../types/user.types';
 import { BackendError } from '..';
 import { router } from 'expo-router';
-
-
-const LoginFormSchema = yup.object({
-  username: yup.string().required("required field"),
-  password: yup.string().required('required field')
-})
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const LoginScreen = () => {
   const { setUser } = useContext(UserContext)
+  const [username, setUsername] = useState<string>()
+  const [password, setPassword] = useState<string>()
   const { mutate, data, isSuccess, isLoading, error } = useMutation
     <AxiosResponse<{ user: IUser, token: string }>,
       BackendError,
       { username: string, password: string, multi_login_token?: string }
     >(Login)
 
+  function handleSubmit() {
+    if (username && password)
+      mutate({ username, password })
+  }
+  useEffect(() => {
+    if (!username) {
+      async function Set() {
+        let uname = await AsyncStorage.getItem('uname')
+        let passwd = await AsyncStorage.getItem('passwd')
+        setUsername(String(uname))
+        setPassword(String(passwd))
+      }
+      Set()
+    }
+  }, [])
 
   useEffect(() => {
+    async function Set() {
+      if (username && password) {
+        await AsyncStorage.setItem('uname', username)
+        await AsyncStorage.setItem('passwd', password)
+      }
+    }
     if (isSuccess) {
       setUser(data.data.user)
       router.replace("/")
+      Set()
     }
   }, [isSuccess])
-
+  
   return (
     <>
       <Snackbar
@@ -46,60 +63,41 @@ const LoginScreen = () => {
         }}>
         {error && error.response.data.message || ""}
       </Snackbar>
+      <ScrollView contentContainerStyle={{ flex: 1, gap: 15, justifyContent: 'flex-start', padding: 10, marginTop: 40 }}>
+        <View style={{ alignItems: 'center', marginBottom: 50 }}>
+          <Image style={{ height: 150, width: 150 }} source={require("../assets/icon.png")} />
+          <Text>Long Lasting safety shoes</Text>
+        </View>
 
-      <Formik
-        initialValues={{ username: "", password: "" }}
-        validationSchema={LoginFormSchema}
-        onSubmit={async (values) => {
-          mutate({
-            ...values,
-            multi_login_token: "akknahkh"
-          })
-        }}
-      >
-        {({ handleChange, handleBlur, handleSubmit, values }) => (
-          <>
+        <TextInput
+          mode="outlined"
+          style={{ borderRadius: 10, borderWidth: 1, borderColor: MD2Colors.blue400, paddingTop: 5 }}
+          outlineStyle={{ display: 'none' }}
+          label="Username,email or mobile"
+          autoCapitalize='none'
+          onChangeText={(text) => setUsername(text)}
+          value={username}
+        />
+        <TextInput
+          mode="outlined"
+          style={{ borderRadius: 10, borderWidth: 1, borderColor: MD2Colors.blue400, paddingTop: 5 }}
+          outlineStyle={{ display: 'none' }}
+          label='Password'
+          autoCorrect={false}
+          autoCapitalize='none'
+          secureTextEntry={true}
+          onChangeText={(text) => setPassword(text)}
+          value={password}
+        />
 
-            <ScrollView contentContainerStyle={{ flex: 1, gap: 15, justifyContent: 'flex-start', padding: 10, marginTop: 40 }}>
-              <View style={{ alignItems: 'center',marginBottom:50 }}>
-                <Image style={{ height: 150, width: 150 }} source={require("../assets/icon.png")} />
-                <Text>Long Lasting safety shoes</Text>
-              </View>
-
-              <TextInput
-                mode="outlined"
-                style={{ borderRadius: 10, borderWidth: 1,borderColor:MD2Colors.blue400, paddingTop: 5 }}
-                outlineStyle={{ display: 'none' }}
-                label="Username,email or mobile"
-                onChangeText={handleChange('username')}
-                onBlur={handleBlur('username')}
-                autoCapitalize='none'
-                value={values.username}
-              />
-              <TextInput
-                mode="outlined"
-                style={{ borderRadius: 10, borderWidth: 1,borderColor:MD2Colors.blue400, paddingTop: 5 }}
-                outlineStyle={{ display: 'none' }}
-                label='Password'
-                autoCorrect={false}
-                autoCapitalize='none'
-                secureTextEntry={true}
-                onChangeText={handleChange('password')}
-                onBlur={handleBlur('password')}
-                value={values.password}
-              />
-
-              {<Button
-                mode="contained"
-                disabled={isLoading}
-                style={{ borderRadius: 10,marginTop:10,padding:10 }}
-                onPress={() => handleSubmit()}>
-                {!isLoading ? "SIGN IN" : "LOGGING IN ..."}
-              </Button>}
-            </ScrollView>
-          </>
-        )}
-      </Formik >
+        {<Button
+          mode="contained"
+          disabled={isLoading}
+          style={{ borderRadius: 10, marginTop: 10, padding: 10 }}
+          onPress={() => handleSubmit()}>
+          {!isLoading ? "SIGN IN" : "LOGGING IN ..."}
+        </Button>}
+      </ScrollView>
     </>
   )
 }
